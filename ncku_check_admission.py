dfa89149449f9ff9355a99d5a85e957c606996e8
DokiDokiPB <20497934+a1091150@users.noreply.github.com>
2022-04-29 15:56:43 +0800

    Add files via upload
    新增檔案

---
+++ ncku_check_admission.py
@@ -0,0 +1,48 @@
+from re import S
+from termios import CSIZE
+from tokenize import String
+import requests
+from bs4 import BeautifulSoup
+
+def getAdmissionList(group_no:String):
+    csie_url = "https://nbk.acad.ncku.edu.tw/netcheckin/index.php"
+
+    params = {"c": "quall_rwd", "m" : "qu"}
+    data = {"exam_id": "2", "group_no" : group_no}
+    req = requests.post(csie_url, params= params, data = data)
+
+    soup = BeautifulSoup(req.text, "html.parser" )
+    # print(soup.prettify())
+
+    admission_list = []
+    wait_list = []
+
+    table = soup.find("table", attrs={"class": "table table-striped"})
+    table_body = table.find("tbody")
+    rows = table_body.find_all("tr")
+    for row in rows:
+        cols = row.find_all("td")
+        content = cols[1].text + " " + cols[3].text + "|" + cols[0].text
+        if "備取" in cols[0].text:
+            wait_list.append(content)
+            break
+
+
+        # if "備取" in cols[0].text:
+            # wait_list.append(content)
+            # ()
+        # else:
+            # admission_list.append("\033[0;33m" + content + "\033[0m")
+            # ()
+
+    title = soup.find("div", attrs={"class": "page-header"}).text
+    print("\033[1;31m" + title + "\033[0m")
+    for cc in admission_list:
+        print(cc)
+    print("")
+    for cc in wait_list:
+        print(cc)
+
+
+getAdmissionList("690") # CSIE
+getAdmissionList("070") # CSIE AI
